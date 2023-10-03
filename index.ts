71334d2b8edf33014a7ad6bd7c6208525cdc9c46
mryhryki <mryhryki@gmail.com>
2023-10-03 18:17:46 +0900

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,1 +1,7 @@
-Deno.serve(() => new Response("Hello, world!"));
+const kv = await Deno.openKv();
+
+Deno.serve(async (request): Promise<Response> => {
+  const key: string = new URL(request.url).searchParams.get("key") ?? "DefaultKey";
+  await kv.set([key], new Date().toISOString());
+  return new Response("Hello, world!");
+});
