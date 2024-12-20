0e96a3d20c1cb9dfb12310d1b61495f0a3aa5bcd
mryhryki <mryhryki@gmail.com>
2024-12-20 18:12:27 +0900

    Update index.ts

--- index.ts
+++ index.ts
@@ -4,10 +4,22 @@ kv.listenQueue(async (msg: unknown) => {
   console.log("Deno Queues:", msg);
 });
 
-Deno.serve(async (request): Promise<Response> => {
+Deno.serve(async (request: Request): Promise<Response> => {
   const key: string = new URL(request.url).searchParams.get("key") ?? "DefaultKey";
+
+  // Deno KV Test
   const uuid = crypto.randomUUID();
   await kv.set([key], uuid);
   await kv.enqueue({ key, uuid }, { delay: 3000 });
-  return new Response("Hello, world!");
+
+  const data = {
+    method: request.method,
+    url: request.url,
+  }
+
+  return new Response(JSON.stringify(data, null, 2), {
+    headers: new Headers({
+      "content-type": "application/json",
+    }),
+  });
 });
